e6851a7ad8f87ebe3ec496369afbacd276cb82da
InsteanAzeros <fctinstean@gmail.com>
2021-07-09 12:46:09 +0200

    exercises form

---
+++ PHP/Day 2 - Date_Forms/Exercises/Form_Exercise_1.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+-- Exercise 1 :
+	
+	1.1 : 
+	Create a basic connection form with email and password input.
+
+	1.2 :
+	Check if the mail contains the '@' symbol thanks to the 'strpos' function.
+	If yes, display 'valid email';
+	If no, display 'invalid email';
+
+	1.3 :
+	When the user validates the form: display a message in red if invalid, show in green if valid.
+
+-- Exercise 2 :
+	
+	Create an HTML form with one input (date picker) and a 'submit' button.
+	When the 'submit' button is clicked, display the difference (in timestamp) between the date of today and the date in the input.
+	Do not worry about what's in the input once the button is clicked.
+*/
---
+++ PHP/Day 2 - Date_Forms/Exercises/Form_Exercise_3.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/*
+    -- Exercise : 
+	
+		- Part 1 :
+	   		Create a page:
+	        - signin.php: User registration page, 
+			with the following fields:
+	            - Last Name
+	            - First name
+	            - E-mail
+	            - Password
+	            - Confirmation of password
+	            - Checkbox "Subscribe to the newsletter"
+
+		- Part 2 :
+			1. Only when the form is being submitted, we will display the summary of what the user entered.
+
+			2. Add validations on the different fields of the form:
+				- The name and the first name are mandatory.
+				- The e-mail must be between 8 and 50 characters
+				- The fields "Password" and "Confirmation" must be identical and have at least 8 characters
+
+			BONUS: Make the form values ​​reappear with each submission, in case of error.
+    */
