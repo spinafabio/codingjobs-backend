0bf9dc42be2496b80b2a3a66d0aefb0f4238e5cc
simon <fctinstean@gmail.com>
2021-07-12 12:21:42 +0200

    Functions exercises

---
+++ PHP/Day 3 - Function_String_Include/Exercises/Function_Exercise_1.php
@@ -0,0 +1,114 @@
+<?php 
+
+echo '<hr>';
+echo '<p style="font-weight: 900"> EXERCISE 1 </p>';
+
+/*
+- Exercise 1
+
+Write a function that:
+    - Takes into parameter two numbers.
+    - Check which is the largest number.
+
+The expected result is this:
+    The first number is larger (if the first number is larger than the second number)
+    The first number is smaller (if the first number is smaller than the second number)
+    The two numbers are identical (if the two numbers are equal)
+
+*/
+
+echo '<hr>';
+echo '<p style="font-weight: 900"> EXERCISE 2 </p>';
+
+/*
+-- Exercise 2
+
+	3.1
+	Write a PHP script that:
+	    - Create an array of John's expenses (do it manually)
+	    - Calculates the sum of John's expenses over the year
+
+	3.2
+	Write a function that will:
+	- Take an expense array as input
+	- Calculate the sum of the expenses of the table
+	- return this sum
+
+*/
+
+echo '<hr>';
+echo '<p style="font-weight: 900"> EXERCISE 3 </p>';
+
+/*
+-- Exercice 3
+
+Write a PHP script that checks if a string is a palindrome.
+A palindrome is a chain of letters whose order of letters remains the same whether read from left to right or from right to left.
+
+Example : 
+"kayak"
+"xanax"
+"poop"
+
+*/
+
+echo '<hr>';
+echo '<p style="font-weight: 900"> EXERCISE 4 </p>';
+
+/*
+-- Exercice 4
+
+Write a function that checks if a number is a prime number.
+A prime number is an integer greater than 1 that can only be divided by itself and 1.
+
+*/
+
+echo '<hr>';
+echo '<p style="font-weight: 900"> EXERCISE 5 </p>';
+
+/*
+
+-- Exercice 5
+Write a htmlImages($src) function that:
+    - takes a string as argument ($src)
+    - display an html <img> tag with $src source
+Example :
+    htmlImages('skate.jpg') 
+    	> Displays <img src='skate.jpg'>
+
+*/
+
+echo '<hr>';
+echo '<p style="font-weight: 900"> EXERCISE 6 </p>';
+
+/*
+-- Exercice 6
+
+Ecrire une fonction qui :
+    - Prend en paramètre deux nombres.
+    - Qui retourne le résultat de la multiplication des deux nombres
+    - Tous les paramètres doivent avoir une valeur par défaut.
+    - Appeler votre fonction avec les nombres 10 et 2.
+    - Appeler votre fonction avec un seul nombre : 4
+
+Write a function that:
+    - Takes two numbers as parameter.
+    - That returns the result of the multiplication of the two numbers
+    - All parameters must have a default value.
+    - Call your function with the numbers 10 and 2.
+    - Call your function with a single number: 4
+*/
+
+echo '<hr>';
+echo '<p style="font-weight: 900"> EXERCISE 7 </p>';
+
+/*
+-- Exercice 7 :
+	Write a PHP function that return the reverse(mirror) of an array.
+	You can use only one other variable (simple, no array).
+	You can only use count() or strlen() function.
+*/
+
+
+
+ ?>
---
+++ PHP/Day 3 - Function_String_Include/Exercises/Function_Exercise_2.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+-- Exercise 1 : 
+	Write a function 'isOrder' that :
+		- Take one array of integer as argument
+		- Checks if the elements of the arrays are ordered in ascending order.
+
+-- Exercise 2 :
+
+	Write a function 'orderArray' that :
+		- Take one array of integer as argument
+		- Return an array which was ordered
+
+-- Exercise 3 :
+
+	Write a function that : 
+		- Take one float $x as argument
+		- Get the integer part of a positive number ($x)
+		- Return this integer part
+
+	Example :
+		integerPart(5.26) // return 5
+		integerPart(10.76) // return 10
