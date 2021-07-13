8f752e37131a3337c4922fb00e2b8eb3a220647f
simon <fctinstean@gmail.com>
2021-07-13 10:51:01 +0200

    TicTac

---
+++ PHP/Day 3 - Function_String_Include/Exercises/Function_Exercise_BONUS.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+
+We have an array of integers, positive, between 1 and $nbElements (nbElements is the number of elements. You can use this variable).
+
+Examples :
+
+	Write a function call 'TicTac'.
+	For each number 'n' of the list, we want the following operations:
+		. if the number is divisible by 3 : display 'Tic'
+		. if the number is divisible by 5 : display 'Tac'
+		. if the number is divisible by 3 AND by 5 : display 'TicTac'
+		. else : display the number 'n'
+		*/
+
+?>
