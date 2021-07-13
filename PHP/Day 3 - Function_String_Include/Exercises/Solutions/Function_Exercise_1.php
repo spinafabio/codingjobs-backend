3ea0d3e826c61e8431033df1ce8ee230a093d25a
Simon <fctinstean@gmail.com>
2021-07-13 08:39:52 +0200

    day 3 solution

--- PHP/Day 3 - Function_String_Include/Exercises/Solutions/Function_Exercise_1.php
+++ PHP/Day 3 - Function_String_Include/Exercises/Solutions/Function_Exercise_1.php
@@ -17,6 +17,17 @@ The expected result is this:
 
 */
 
+function checkNumber($num1, $num2)
+{
+	if ($num1 > $num2)
+		echo 'The first number is higher';
+	else if ($num1 < $num2)
+		echo 'The second number is higher';
+	else
+		echo 'Both numbers are identical';
+}
+
+
 echo '<hr>';
 echo '<p style="font-weight: 900"> EXERCISE 2 </p>';
 
@@ -31,13 +42,27 @@ echo '<p style="font-weight: 900"> EXERCISE 2 </p>';
 	3.2
 	Write a function that will:
 	- Take an expense array as input
-	- Calculate the sum of the expenses of the table
+	- Calculate the sum of the expenses of the array
 	- return this sum
 
 */
 
+// Declare the function : 
+function sum_expenses($myArray)
+{
+	$total = 0;
+
+	foreach ($myArray as $value) {
+		$total = $total + $value;
+	}
+
+	echo 'Total is : ' . $total;
+}
 
 
+// Call the function : 
+$array = array("Salad" => "1.03", "Tomato" => "2.3", "Oignon" => "1.85", "Red cabbage" => "0.85");
+sum_expenses($array);
 
 echo '<hr>';
 echo '<p style="font-weight: 900"> EXERCISE 3 </p>';
@@ -55,15 +80,16 @@ Example :
 
 */
 
-function isPalindrome($str)
+function isPalindrome($word)
 {
-	// DO NOT DO THIS (if you can avoid): 
-	$reverse = strrev($str);
+	$length = strlen($word);
 
-	if ($reverse == $str)
-		return 'Palindrome !';
-	else
-		return 'Not a palindrome';
+	for ($i = 0; $i < $length / 2; $i++) {
+		if ($word[$i] != $word[$length - 1 - $i])
+			return 'Its not a palindrome !';
+	}
+
+	return 'Its a palindrome ! ';
 }
 
 echo '<hr>';
@@ -76,6 +102,20 @@ Write a function that checks if a number is a prime number.
 A prime number is an integer greater than 1 that can only be divided by itself and 1.
 
 */
+
+function isPrime($number)
+{
+
+	for ($i = 2; $i < $number; $i++) {
+		// Check if $number is divisible by $i
+		if ($number % $i == 0)
+			return 'Its NOT a prime number';
+	}
+
+	return 'It is a prime number';
+}
+
+echo isPrime(50);
 
 echo '<hr>';
 echo '<p style="font-weight: 900"> EXERCISE 5 </p>';
@@ -121,4 +161,16 @@ echo '<p style="font-weight: 900"> EXERCISE 7 </p>';
 	Write a PHP function that return the reverse(mirror) of an array.
 	You can use only one other variable (simple, no array).
 	You can only use count() or strlen() function.
+
+
+	HINT : How to swith two values
+	Giving 2 variables $x and $y
+	$x = 5
+	$y = 10
+
+	Swith the two values.
+
+	$z = $x // Saving the first value (5)
+	$x = $y // x = 10
+	$y = $z // y = 5
 */
