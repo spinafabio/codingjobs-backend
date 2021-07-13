33376de2753dc911bf03c6086d7eee608a588bd3
simon <fctinstean@gmail.com>
2021-07-13 14:35:38 +0200

    database

---
+++ PHP/Day 4 - Database_Cookies/Exercises/PHP_Database_Exercise_2.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+	This exercise is based on the exercise : 'PHP_Database_Exercise_1'.
+	
+	Edit the 'movies.php' page
+	Now this page will have a search box -> create an input:text
+	
+	When the user is typing something and click on 'search' button, you should display all the movies matching the search.
+
+	The search is based on the name of the movie. The user don't have to type the specific name.
+
+	For example :
+		User types 'jur', it should display all movies containing 'jur' (ex: jurassic park).
+
+	-- STEP 1 : Create the form
+	-- STEP 2 : Check if the button was clicked :
+		** If it wasn't, search all movies
+		** If it was, search for the specific movie
+	-- STEP 3 : Display (already done)
+
+
+
+?>
