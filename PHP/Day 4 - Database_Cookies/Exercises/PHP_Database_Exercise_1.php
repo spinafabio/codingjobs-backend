8ca7ed0af949d6d0f2822389e7de6d4ef4a3e088
simon <fctinstean@gmail.com>
2021-07-13 14:23:09 +0200

    database

---
+++ PHP/Day 4 - Database_Cookies/Exercises/PHP_Database_Exercise_1.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+	In this exercise, we'll use the 'moviedb' database.
+	
+	1. Create a page 'index.php'.
+	This page will display a nice message like 'Welcome to the freakin movie website' (or whatever)
+	Also display only the last three movies (ordered by date of release).
+
+	2. Create a page 'movies.php'.
+	This page will display all the movies (poster first, then title and release year).
+	
+	4. Create a page 'movie.php'.
+	This page is a descriptive page for each movie. It'll display the poster, the title, the release year but also the director's name.
+	This page will have to use the GET method to get the id of the movie you want to display.
+	So in your adress bar it'll look like this : 'localhost/movie.php?id=1'
+	You have to get the id of the movie to know which movie to display.
+	
+	5. Edit the page 'movies.php'
+	Make the title of each movie clickable. It'll redirect to the related descriptive movie page.
+
+*/
