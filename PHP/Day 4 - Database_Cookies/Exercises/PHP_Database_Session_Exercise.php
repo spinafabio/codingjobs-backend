ee4dc3dfb3a5ac0c7e2e8b0444ba5e0fdccf5ce0
simon <fctinstean@gmail.com>
2021-07-13 14:40:17 +0200

    database

---
+++ PHP/Day 4 - Database_Cookies/Exercises/PHP_Database_Session_Exercise.php
@@ -0,0 +1,40 @@
+<?php
+/*
+	Let's assume you have an online shopping website.
+
+	Step 1 :
+		Import the 'users.sql' file into PHPMYADMIN.
+		You have to choose a DB first (you can use movie_db or create a new one).
+
+		Some users will be imported into your database.
+		You can insert users manually from PHPMYADMIN interface if you want.
+
+    Step 2: 
+	    Create a page 'login.php'.
+	    Inside this page, create a form to login into your beautiful website.
+
+	    The form will ask for : email, password.
+
+    Step 3: 
+		Once the user submit the login form, you have to :
+			- Check if email is not empty.
+			- Open a session.
+			- In this session, save the email adress.
+
+	Step 4:
+		Create a page 'account.php'.
+
+		If a user is trying to access that page :
+			- Check if the user logged in before 
+			- If he's not log-in you need to redirect to the login page.
+			- If he logged-in, display 'Hello, username'
+
+		To redirect in php : Look for header('location : ....') on google
+
+    Step 5 (BONUS): 
+		
+		Add a 'logout' button.
+		When the user click, the user should be redirected to the login page and should now be log out.
+
+
+ */
---
+++ PHP/Day 4 - Database_Cookies/Exercises/users.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le :  mer. 28 oct. 2020 à 14:05
+-- Version du serveur :  5.7.25
+-- Version de PHP :  7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `movie_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'simon', 'simon@gmail.com', '1234'),
+(2, 'david', 'david@gmail.com', '4321');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
