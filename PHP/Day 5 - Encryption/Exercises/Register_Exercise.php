a2c5009009f6d45944410666fffa3fba2b447525
simon <fctinstean@gmail.com>
2021-07-14 14:44:23 +0200

    encryption

---
+++ PHP/Day 5 - Encryption/Exercises/Register_Exercise.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+
+    Exercise :
+
+    This exercise is based on the 'Login Exercise'.
+
+    In this exercise, you'll have to create the register process.
+
+    - Step 1 :
+        Create a 'register.php' file
+        This file contains a form with those input :
+            * username
+            * email
+            * password
+            * submit btn
+    
+    - Step 2 :
+        If the form is submitted, you have to insert the new user in the DB.
+        
+        But first, you have to check :
+            * username, mail and password must not be empty !
+            * mail must be a valid one
+            * password must be hashed
+
+        Display a different message for each validation/error.
+
+    - Step 3 :
+        If everything is good, insert the new user in the DB !
+
+    - Step 4 :
+            Edit the login.
+            Now the login should verify if the password match the hash one !!
+
+            Be carefull, your old users dont have a hash password.
+    */
