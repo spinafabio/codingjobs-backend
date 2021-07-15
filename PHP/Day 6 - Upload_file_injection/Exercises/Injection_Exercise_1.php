353bc2ed87686c58dc863d07486940a764249d54
simon <fctinstean@gmail.com>
2021-07-15 09:34:25 +0200

    day 6 exercises

---
+++ PHP/Day 6 - Upload_file_injection/Exercises/Injection_Exercise_1.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+	
+	Based on the 'Register_Exercise'.
+
+	Put in place a basic security check in the login/register pages.
+	Remove white spaces, Avoid injections, special chars....
+*/
+
+?>		
