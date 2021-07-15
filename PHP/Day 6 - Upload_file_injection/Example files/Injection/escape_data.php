9ac14b3b1620067faafd453377f0f7e5d358b17d
simon <fctinstean@gmail.com>
2021-07-15 09:35:35 +0200

    day 6 exercises

--- PHP/Day 6 - Upload_file_injection/Example files/Injection/escape_data.php
+++ PHP/Day 6 - Upload_file_injection/Example files/Injection/escape_data.php
@@ -1,26 +1,36 @@
 <?php
 
 /*
-    Cleaning data.
+    Cleaning form data.
+
+    User data can be manipulated in many ways by malicious person through the form.
+
+    You have to reduce the risk for the website : 
+        - Clean data
+        - Validate data
+        - Escape data before sending to the DB
+        - Escape before sending to the client
+    
+    
+    Let's have an example (movie exercise).
+    For detail page we use GET method to pass the id of the movie
+    
+    localhost/movie.php?id=5
+
+    A user can try to acces the page doing something like this :
+    movie.php?id=1;select * from users;
 */
 
-$id = $_GET['id'];
+// How to avoid this : for example, I can cast the variable to int
+$id = (int) $_GET['id'];
+// Try to convert to int, if its not a number, it will return 0
 
-$query = 'SELECT * FROM movies WHERE id = ' . $id;
+if ($id != 0) {
+}
 
 /*
-use is_numeric() to check if its a string containing only numeric values
-
-if (is_numeric($id))
-    echo 'Its a number';
-else
-    echo 'Its not a number';
+    All form data is processed
+    Initially like strings, so, casting
+    the variables sent can be easily cleaned
+    the id sent by the user.
 */
-
-// Cast the variable to int
-$id = (int) $_GET['id'];
-
-// If you get 0, it means its not a number
-if ($id > 0) {
-    // execute the query
-}
