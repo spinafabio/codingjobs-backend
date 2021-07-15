b178f8f44ab15a91310ccea96ad164053a3828bd
simon <fctinstean@gmail.com>
2021-07-15 12:02:31 +0200

    afternoon exercise

---
+++ PHP/Day 6 - Upload_file_injection/Exercises/Afternoon_Exercise_1.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+	This exercise is based on the 'movies.php' page that display all movies.
+	Check 'Day4 - Exercises' for the movies.php page.
+	
+	Edit the page.
+	Now this page will have a search box -> create an input:text
+	
+	When the user is typing something and click on 'search' button, you should display all the movies matching the search.
+
+	The search is based on the name of the movie. The user don't have to type the specific name.
+
+	For example :
+		User types 'jur', it should display all movies containing 'jur' (ex: jurassic park).
+
+		
+
+	-- STEP 1 : Create the form
+	-- STEP 2 : Check if the button was clicked :
+		** If it wasn't, search all movies
+		** If it was, search for the specific movie
+	-- STEP 3 : Display (already done)
+
+
+
+?>
---
+++ PHP/Day 6 - Upload_file_injection/Exercises/Afternoon_Exercise_2.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+	This exercise is based on the 'Afternoon_Exercise1.php' file.
+	
+	You have to create a new page : 'new-movie.php'.
+
+	This page will contain a form to insert/create a new movie on the database.
+	You have to take care of all the fields (poster also).
+
+	Follow those steps : 
+		1. Create the form to match all attributes/columns from movies table.
+		2. Do some validation (not empty...)
+		3. If everything's ok, try to upload the file/poster on the server.
+		4. At the end, insert the movie inside the DB.
+
+?>
