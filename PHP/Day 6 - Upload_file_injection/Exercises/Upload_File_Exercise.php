5f2768e36bba2cd96672c3277746b75e6686d3f1
simon <fctinstean@gmail.com>
2021-07-15 11:24:35 +0200

    day 6 exercise

---
+++ PHP/Day 6 - Upload_file_injection/Exercises/Upload_File_Exercise.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+	
+	Based on the 'Register_Exercise'.
+
+	Edit the 'register.php' page.
+
+	Now, the user can also upload a profile image.
+	The image is not mandatory.
+
+	For this you have to follow those steps : 
+		1. On PHPMYADMIN, edit the 'user' table and add a new attribute : poster.
+		2. Add the input to upload a file on the register form
+		3. Create the PHP code that will save the file on the server ('uploads' folder)
+		4. Grab the name of the image (only the name) and save it as user 'poster' 
+*/
+
+?>		
