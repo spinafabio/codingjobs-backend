f9c037ea088264979ef8beed56762e42315eb647
simon <fctinstean@gmail.com>
2021-07-19 13:39:30 +0200

    pagination

---
+++ PHP/Day 7 - Files/Exercises/Product_Exercise_1.php
@@ -0,0 +1,25 @@
+<?php 
+		/*
+
+        -- Exercise : 
+
+        - Step 1 :
+	        Import 'online_shopping.sql' file on PHPMYADMIN.
+	        This will create a new database with one table 'products'.
+
+        - Step 2 :
+	        Create a page 'products.php'.
+	        This page will display all the products, with the picture first, then name, category and price.
+
+        - Step 3 : 
+			Edit the 'products.php' page.
+			Now this page should implements pagination.
+			Display 3 products per page.
+			You should be able to change pages by changing the URL parameter.
+
+		- Step 4 :
+			Create button so that we can navigate through all the products.
+
+    */
+
+?>
---
+++ PHP/Day 7 - Files/Exercises/Product_Exercise_2.php
@@ -0,0 +1,28 @@
+<?php 
+	
+	/*
+        -- Exercise : 
+
+        In this exercise we will implements the cart principle.
+
+        - Step 1 :
+	        Edit the 'products.php' page
+	        Next to each product, add a button 'Add to cart'.
+
+        - Step 2 :
+        	When you click on the button it should add this product to cart.
+        	You need, at least, the name and the price.
+
+        - Step 3 :
+			Create an new item in your nav/menu looking like this : cart(x)
+			x will be the number of products in your cart.
+
+        - Step 4 :
+	        Each time a product is added, update the cart number of your nav/menu.
+
+		- Step 5 : 
+			Handle the quantity if you click multiple time on 'add to cart' for the same product.
+
+    */
+
+?>
