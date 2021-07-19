8b91a11c0d131b95f194b1508bc00c5e5294cead
simon <fctinstean@gmail.com>
2021-07-19 09:49:48 +0200

    Exercises on files

---
+++ PHP/Day 7 - Files/Exercises/File_Exercise_2.php
@@ -0,0 +1,10 @@
+<?php
+
+	/*
+		1. Read the file : 'users.txt'
+
+		2. Retrieve, in PHP, all the users as an array.
+		For Each element of the array (meaning each users), you should save username and email.
+
+		3. Display the array with a var_dump()
+	 */
