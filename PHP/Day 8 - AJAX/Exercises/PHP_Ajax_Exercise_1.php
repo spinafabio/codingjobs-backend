cbd94b915f6fbca05f7d67fba9d116113907a5e7
Simon <fctinstean@gmail.com>
2021-07-20 13:35:17 +0200

    exercise

--- PHP/Day 8 - AJAX/Exercises/PHP_Ajax_Exercise_1.php
+++ PHP/Day 8 - AJAX/Exercises/PHP_Ajax_Exercise_1.php
@@ -4,7 +4,7 @@
 
 	-- Exercise : 
 
-	In this exercise, you will use the 'moviedb' database.
+	In this exercise, you will use the 'movie_db' database.
 
 	- Step 1 :
 		Create two pages: 'new-movie.html' and 'insert-movie.php'
@@ -12,11 +12,13 @@
 	- Step 2: 'new-movie.html' should contain a form to insert a new movie.
 		You should create all the necessary inputs.
 
+		For now, the director id should be an input:number.
+		
 	- Step 3 :
 		When clicking on submit button you must do an AJAX call to the 'insert-movie.php'
 		This script will check if all the inputs are valid and try to insert a new movie in the Database.
 
-	- Step 3 :
+	- Step 4 :
 		If there is a problem with the form display an error message (color red)
 		If everything went fine, display a success message (color green)
 
