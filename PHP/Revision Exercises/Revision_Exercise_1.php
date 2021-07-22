a72d0b3e06c5032ea43950452c547bca485b77fa
simon <fctinstean@gmail.com>
2021-07-22 08:48:50 +0200

    Revision exercises

---
+++ PHP/Revision Exercises/Revision_Exercise_1.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+	1. Display the type of each variable at the end of the script (using only one function)
+	2. Display the type AND the value of each variable (using only one function)
+*/
+
+/*
+	Show the date (in a nice format) for :
+		- Today
+		- Tomorrow
+		- The same date than today one month later
+*/
+
+/*
+	Create an array with the numbers from 1 to 100 : Create it dynamically using a loop.
+	Once it has been created, display it in a HTML list (ul/li)
+
+	Use one loop to create the array.
+	Use a second loop to display it.
+*/
+
+/*
+	Practice query using the moviedb :
+	1. Connect to DB and choose the moviedb
+	2. Go to SQL tab and run a query to get all movies by George Lucas
+	3. Display the movies in a HTML <table>
+		You should display title, views, the poster and the name of the director.
+*/
---
+++ PHP/Revision Exercises/Revision_Exercise_2.php
@@ -0,0 +1,35 @@
+<?php 
+
+/*
+    -- Exercise 1 :
+    
+    Thanks to that array : 
+    $array = array(
+        'lastname' => 'simon',
+        'firstname' => 'simon',
+        'phone' => '+3526455748'
+        'mail' => 'simon@simon.fr',
+        'address' => '6 rue de la gare',
+        'city' => 'Mondelange'
+    );
+
+    Using a loop, display the content of this array (keys and values)
+*/
+
+
+
+
+/* -- Exercise : 
+	1 :
+	Create a function 'multiplicationTable'
+	2 :
+	This function should have on argument $x
+	3:
+	The function should return the multiplication table of $x (from 1 to 20).
+	4 :
+	Create a form with one input.
+	When you validate this form, it should display the multiplication table of the number you entered (using the function).
+	You have to check if the value is correct (no float and no string, ONLY integer).
+	5 :
+	Display the multiplication table in a nice HTML format table style.
+*/
