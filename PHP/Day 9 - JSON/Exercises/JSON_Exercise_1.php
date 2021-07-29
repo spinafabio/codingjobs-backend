3e7469839cab4873f56af88f8a7a029c2bbacb8d
simon <fctinstean@gmail.com>
2021-07-29 10:17:50 +0200

    json

---
+++ PHP/Day 9 - JSON/Exercises/JSON_Exercise_1.php
@@ -0,0 +1,24 @@
+<?php 
+
+/*
+	Exercise : 
+
+	Step 1 - Create a form that ask the user to enter name and email.
+
+	Step 2 - When the user submit the form you must : 
+		2.1 : Check in the 'users.json' file if the user exists.
+		2.2 : If it does, display a welcome message with name and mail.
+		2.3 : If it does not you have to save the user and display "Welcome new member!"
+
+
+	Step 2.3 - Help
+
+		For this step, you have to follow those steps : 
+			* Grab the content of the json file 
+			* Deserialize it
+			* Put the new user in the PHP array
+			* Serialize it back
+			* Save the content in the file
+
+ ?>
+
