03341076f8d5a502da5d647beed56e69068ee28d
Simon <fctinstean@gmail.com>
2021-07-29 13:29:08 +0200

    exercises

---
+++ PHP/Day 9 - JSON/Exercises/JSON_Exercise_3.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+	Exercise : 
+
+	This exercise is based on "Day 8 / ajax_exercise_1.php".
+    Topic : inserting a new movie in the DB.
+
+	You can grab the solution and starts from there.
+
+	The goal of the exercise is to use AJAX with JSON on this one.
+
+    AJAX is already done, we have to add JSON.
+
+    In the php file, use JSON in order to transfer data to Javascript.
+    Use Javascript to display all movies.
+
+
